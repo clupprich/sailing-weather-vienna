d0fd7c394625076ead7faae6fad6f6af9a13099d
Christoph Lupprich <christoph@luppri.ch>
2014-08-11 08:10:04 +0200

    Add weather icons and more styling, also improve returned data

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require('express')
   , bodyParser = require('body-parser')
+  , sass = require('node-sass')
   , path = require('path')
   , favicon = require('static-favicon')
   , logger = require('morgan')
@@ -7,10 +8,18 @@ var express = require('express')
 
 var app = express();
 
+app.set('view engine', 'ejs');
+
 app.use(favicon());
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
+app.use(sass.middleware({
+  src: path.join(__dirname, 'sass'),
+  dest: path.join(__dirname, 'public'),
+  debug: true,
+  outputStyle: 'compressed'
+}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
@@ -27,6 +36,7 @@ app.use(function(req, res, next) {
 if (app.get('env') === 'development') {
   app.use(function(err, req, res, next) {
     res.status(err.status || 500);
+    console.log(err);
     res.render('error', {
       message: err.message,
       error: err
--- routes/weather.js
+++ routes/weather.js
@@ -4,6 +4,7 @@ var express = require('express')
   , redis = require('redis')
   , url = require('url')
   , q = require('q')
+  , util = require('util');
 
 var router = express.Router();
 var redisURL = url.parse(process.env.REDISCLOUD_URL || 'http://127.0.0.1:6379');
@@ -18,19 +19,42 @@ loadData = function() {
   request.get('https://www.wien.gv.at/wetter/segeln/', function(error, response, body) {
     if (!error && response.statusCode == 200) {
       var data = [];
+      var now = new Date();
+      var dayWrap = false;
+      var prevTime;
 
       $ = cheerio.load(body);
       $('.BDE-table-frame tr').each(function(_, row) {
         var columns = $(this).find('td').get();
 
         if (columns.length > 0) {
+          var time = new Date(util.format('%s-%s-%s %s', now.getFullYear(), now.getMonth() + 1, now.getDate(), $(columns[0]).text().match(/\d{2}:\d{2}/)[0]));
+          //var time = $(columns[0]).text().match(/\d{2}:\d{2}/)[0];
+
+          if (!dayWrap && prevTime) {
+            if (prevTime.getHours() > time.getHours()) {
+              dayWrap = true;
+            }
+          }
+
+          if (dayWrap) {
+            time.setDate(now.getDate() + 1);
+          }
+
+          var wind = parseFloat($(columns[1]).text().match(/\d+.\d+/)[0]);
+          var direction = $(columns[2]).text().match(/\w+/)[0];
+          var temperature = parseFloat($(columns[3]).text().match(/-*\d+.\d+/)[0]);
+          var weather = $(columns[4]).html().match(/alt="([\w\s]+)"/)[1].trim().toLowerCase();
+
           data.push({
-            time:        $(columns[0]).text().match(/\d{2}:\d{2}/)[0],
-            wind:        $(columns[1]).text().match(/\d+.\d+/)[0],
-            direction:   $(columns[2]).text().match(/\w+/)[0],
-            temperature: $(columns[3]).text().match(/-*\d+.\d+/)[0],
-            weather:     $(columns[4]).html().match(/alt="([\w\s]+)"/)[1].trim()
+            time:        time.toISOString(),
+            wind:        wind,
+            direction:   direction,
+            temperature: temperature,
+            weather:     weather
           });
+
+          prevTime = time;
         }
       });
 
